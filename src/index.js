1ef516da37510f53ce5613e18543944ab8d2a3f6
Perry Kaufman <perry.kaufman@outlook.com>
2022-10-01 20:00:40 -0700

    basic count and set functionality

--- src/index.js
+++ src/index.js
@@ -1,23 +1,54 @@
-const setFieldElement = document.getElementById("setValue")
-const configElement = document.getElementById("configSection")
-const displayElement = document.getElementById("outputDisplay")
+const setFieldElement = document.getElementById("setValue");
+const configElement = document.getElementById("configSection");
+const displayElement = document.getElementById("outputDisplay");
+const configToggleElement = document.getElementById("configOpen");
 
 //Listeners
 document.getElementById("clickerButton").onclick = function() {
-    console.log("click");
+    var count = localStorage.getItem("count");
+    set(++count)
 }
 
-document.getElementById("configOpen").onclick = function(){
-    console.log("toggle")
+configToggleElement.onclick = function(){
+    if (configElement.style.display == "none") {
+        configElement.style.display = "block"
+        configToggleElement.innerHTML = "config (hide)"
+    } else {
+        configElement.style.display = "none"
+        configToggleElement.innerHTML = "config (show)"
+    }
 }
 
 document.getElementById("setButton").onclick = function(){
-    console.log("set")
+    var num = Number(setFieldElement.value);
+
+    if (Number.isInteger(num) && num > -1) {
+        set(num)
+    } else {
+        //invalid number
+        console.log("invalid number");
+    }
+    setFieldElement.value = "";
 }
 
 init();
 
 //Functions
 function init(){
-    console.log("hello world");
+    
+    //Initialize Count
+    var count = localStorage.getItem("count")
+    if (count === null) {
+        localStorage.setItem("count", 0)
+        displayElement.innerHTML ="0"
+    } else {
+        displayElement.innerHTML = count
+    }
+
+    //Hide Config
+    configElement.style.display = "none"
+}
+function set(num) {
+    localStorage.setItem("count", num);
+    displayElement.innerHTML = num
 }
